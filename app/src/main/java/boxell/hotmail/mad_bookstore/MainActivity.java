52b4a412ea51683fb246bcf5af80bf79fe4dfa0e
Boxy1 <boxell@hotmail.com>
2018-03-22 14:50:37 +0000

    push 1

---
+++ app/src/main/java/boxell/hotmail/mad_bookstore/MainActivity.java
@@ -0,0 +1,69 @@
+package boxell.hotmail.mad_bookstore;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private Button Create;
+    private int counter = 5;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Info = (TextView)findViewById(R.id.tvAttempts);
+        Login = (Button)findViewById(R.id.btnLogIn);
+        Create = (Button)findViewById(R.id.btnCreate);
+
+         Info.setText("No of attempts remaining: 5");
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+        Create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, InitialActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void validate (String userName,  String userPassword){
+        if ((userName.equals("Admin")) && (userPassword.equals("password"))) {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }else{
+            counter--;
+
+            Info.setText("No of attempts remaining: " + String.valueOf(counter));
+
+            if (counter == 0){
+                Login.setEnabled(false);
+            }
+        }
+    }
+
+}
